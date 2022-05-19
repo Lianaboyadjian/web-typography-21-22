039263f6237330a3cfae1232f31b8c62a51c6f69
Lianaboyadjian <74240003+Lianaboyadjian@users.noreply.github.com>
2022-05-19 22:57:43 +0200

    css en html

--- closed-captions/sounds.js
+++ closed-captions/sounds.js
@@ -1,15 +1,19 @@
 // Hier kan je tijden toevoegen in secondes
-// Op die mommenten komt er een class op de body.
+// Op die momenten komt er een class op de body.
 sounds = [
-	0,
-	9,
-	12,
-	15,
-	16.5,
-	24,
-	25.8,
-	31,
-	32,
-	34,
-	86
+    0,
+    9,
+    12,
+    15,
+    16.5,
+    24,
+    25.8,
+    31,
+    32,
+    34,
+    75.5,
+    86,
+    100,
+    102.5,
+    131
 ];
